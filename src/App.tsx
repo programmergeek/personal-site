043c836fd71ad50e5d4bba95d0f4e642851893a4
programmergeek <programergeek@gmail.com>
2021-10-16 09:36:29 +0200

    testing and adjusting css

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="banner-header">This is a banner header</p>
+      <p className="header">This is a header</p>
+      <p className="header highlight">This is a highlighted header</p>
+      <button className='btn btn-primary font-primary'>Click me</button>
+      <button className='btn btn-secondary font-primary'>Click me</button>
+      <p className='p'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum sequi neque nostrum expedita iure id quo animi exercitationem facere aspernatur quas, vitae doloribus quia beatae labore repellat sint culpa eveniet?</p>
     </div>
   );
 }
