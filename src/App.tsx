6072c37db43f8a9ac07afefdec70dfde5e5eec8f
programmergeek <programergeek@gmail.com>
2021-10-22 12:55:20 +0200

    [react/jsx] created jsx elements to tests d-flex and center classes

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,12 @@ function App() {
       <p className='p'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum sequi neque nostrum expedita iure id quo animi exercitationem facere aspernatur quas, vitae doloribus quia beatae labore repellat sint culpa eveniet?</p>
       <input type="text" className="text-input input-field font-secondary" placeholder='Example' />
       <textarea name="message" placeholder='Message' className='text-input text-area font-secondary'></textarea>
+      <div className="d-flex center">
+        <button className="btn btn-secondary font-primary">One</button>
+        <button className="btn btn-secondary font-primary">Two</button>
+        <button className="btn btn-secondary font-primary">Three</button>
+        <button className="btn btn-secondary font-primary">Four</button>
+      </div>
     </div>
   );
 }
