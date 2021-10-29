ef8d4dbf39f17360ae404bcff98391a687f56f5e
programmergeek <programergeek@gmail.com>
2021-10-29 17:13:19 +0200

    [css/new] classes for text positioning and flex direction

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
-import { HeroBanner, Navbar } from "./Components";
+import { AboutMe, HeroBanner, Navbar } from "./Components";
 
 function App() {
   return (
     <div className="App">
       <HeroBanner />
       <Navbar />
+      <AboutMe />
     </div>
   );
 }
