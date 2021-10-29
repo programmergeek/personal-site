9fea0794d67d8292c352b8d276be8a361d0ea682
programmergeek <programergeek@gmail.com>
2021-10-29 21:12:52 +0200

    [react/component/edit] changed my personal description

--- src/Components/AboutMe.tsx
+++ src/Components/AboutMe.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const AboutMe: React.FC = () => {
   return (
-    <div className="d-flex d-col center">
+    <div className="d-flex d-col center" id="about-me">
       <h1 className="header highlight" style={{ marginTop: 62 }}>
         Hey, I{"'"}m Will.
       </h1>
@@ -13,7 +13,12 @@ export const AboutMe: React.FC = () => {
           alignSelf: "center",
         }}
       >
-        I hope you{"'"}re having an amazing day.
+        I first fell in love with coding and problem solving in highschool and
+        now I want to make a profession out of it. I believe that team work
+        makes the dream work and love working with other people, hearing their
+        opinions, and making something to improve our everyday lives. My hobbies
+        include gaming, reading, and sitting down to watch some shows or movies
+        with the people I care about.
       </p>
     </div>
   );
